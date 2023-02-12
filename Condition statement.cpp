8fbbb657465adb66ebf8610745e19531fe48c9f7
Adham Amr <104816206+Adham-amr-1@users.noreply.github.com>
2023-02-12 11:55:08 +0200

    Create Condition statement.cpp

---
+++ Condition statement.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n>9){
+        cout<<"Greater than 9";
+    }
+    else{
+        switch(n){
+            case 1: cout<<"one";break;
+            case 2: cout<<"two";break;
+            case 3: cout<<"three";break;
+            case 4: cout<<"four";break;
+            case 5: cout<<"five";break;
+            case 6: cout<<"six";break;
+            case 7: cout<<"seven";break;
+            case 8: cout<<"eight";break;
+            default: cout<<"nine";
+        }
+    }
+    return 0;
+}
+
